d573ef27c07b45305e3996718945321484f8d6ac
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2020-05-05 04:52:09 +0300

    typo

--- src/TheClimbing/RPGLike/Players/PlayerManager.php
+++ src/TheClimbing/RPGLike/Players/PlayerManager.php
@@ -51,7 +51,7 @@
         }
 
 
-        public static function getCachedPlayer()
+        public static function getCachedPlayers()
         {
             return RPGLike::getInstance()->getConfig()->getNested('Players');
         }
