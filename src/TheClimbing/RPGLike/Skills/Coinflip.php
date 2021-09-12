57846ae87b8f3b7608db0f1a65f1a1aab2a9a61a
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2021-09-12 13:51:52 +0300

    This should fix coinflip being proced every time

--- src/TheClimbing/RPGLike/Skills/Coinflip.php
+++ src/TheClimbing/RPGLike/Skills/Coinflip.php
@@ -7,6 +7,7 @@ namespace TheClimbing\RPGLike\Skills;
 
 
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use TheClimbing\RPGLike\Players\RPGPlayer;
 use function rand;
 
@@ -26,13 +27,12 @@ class Coinflip extends BaseSkill
     public function setCritChance(EntityDamageByEntityEvent $event)
     {
         $damage = $event->getBaseDamage();
-        if (rand(0, 99) < $this->config[$this->getSkillLevel()]) {
-            $event->setBaseDamage($damage * 1.5);
+        if (rand(0, 99) < $this->config[$this->getSkillLevel()]['chance']) {
+            $event->setModifier($damage * 1.5, EntityDamageEvent::CAUSE_ENTITY_ATTACK);
             $player = $event->getDamager();
             if ($player instanceof RPGPlayer){
                 $this->transmitProcMessage($player);
             }
-
         }
     }
 }
