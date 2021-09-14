fdfe7fc84ec15f6b9aefa3ab7b206fffadc2bfb4
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2021-09-14 21:52:12 +0300

    This needs extensive testing

--- src/TheClimbing/RPGLike/Players/RPGPlayer.php
+++ src/TheClimbing/RPGLike/Players/RPGPlayer.php
@@ -44,7 +44,7 @@ class RPGPlayer extends Player
     private float $defModifier = 0.1;
     private int $defBonus = 1;
     private int $dex = 1;
-    private float $dexModifier = 0.1;
+    private float $dexModifier = 0.001;
     private $dexBonus = 1;
     private \pocketmine\utils\Config $config;
 
