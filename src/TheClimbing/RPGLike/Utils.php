78c27bb234f8018f1643847048fd5f5350873446
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2021-12-09 17:17:56 +0200

    Update Utils.php
    lay down some work for item tiers

--- src/TheClimbing/RPGLike/Utils.php
+++ src/TheClimbing/RPGLike/Utils.php
@@ -4,6 +4,8 @@
 namespace TheClimbing\RPGLike;
 
 
+use pocketmine\item\ItemIds;
+
 class Utils
 {
 
@@ -29,5 +31,42 @@ class Utils
             }
         }
         return $messages;
+    }
+    public static function translateStringToItem(string $item) : array {
+        $items = [
+          'stone_sword' => [ItemIds::STONE_SWORD,0,'Uncommon Sword',['damage'=>1]] ,
+          'stone_axe' => [ItemIds::STONE_AXE,0,'Uncommon Axe',['damage' => 1]],
+          'stone_picaxe' => [ItemIds::STONE_PICKAXE,0,'Uncommon Picaxe',['damage' =>1]],
+          'stone_shovel' => [ItemIds::STONE_SHOVEL,0,'Uncommon Shovel', ['damage' => 1]],
+          'iron_sword' => [],
+          'iron_axe' => [],
+          'iron_picaxe' => [],
+          'iron_shovel' => [],
+          'gold_sword' => [],
+          'gold_axe'=>[],
+          'gold_picaxe' => [],
+          'gold_shovel' => [],
+          'diamond_sword' => [],
+          'diamond_axe' => [],
+          'diamond_picaxe' => [],
+          'diamond_shovel' => [],
+            'leather_helmet' => [],
+            'leather_chestplate' => [],
+            'leather_leggings' => [],
+            'leather_boots' => [],
+            'iron_helmet'=> [],
+            'iron_chestplate' => [],
+            'iron_leggings' => [],
+            'iron_boots' => [],
+            'gold_helmet' => [],
+            'gold_chestplate' => [],
+            'gold_leggings' => [],
+            'gold_boots' => [],
+            'diamond_helmet' => [],
+            'diamond_chestplate' => [],
+            'diamond_leggings' => [],
+            'diamond_boots' => []
+        ];
+        return [];
     }
 }
