aaf0c6c56069c29ff0de31bdbb590d2264a4e6df
M.Ivanov <milko.hristov.ivanov65@gmail.com>
2021-12-09 23:51:40 +0200

    Items should move away from utils
    Just a temporary solution to see if everything works out

--- src/TheClimbing/RPGLike/Items/ItemFactory.php
+++ src/TheClimbing/RPGLike/Items/ItemFactory.php
@@ -3,7 +3,8 @@
 namespace TheClimbing\RPGLike\Items;
 
 use pocketmine\item\Item;
-use pocketmine\item\TieredTool;
+
+use TheClimbing\RPGLike\Utils;
 
 class ItemFactory
 {
@@ -15,7 +16,6 @@ class ItemFactory
             self::$items[$item->getCustomName()] = $item;
         }
     }
-
     public static function getItem(string $itemName)
     {
         if (array_key_exists($itemName, self::$items)) {
@@ -23,18 +23,10 @@ class ItemFactory
         }
         return false;
     }
-
     public static function createItems(array $tiers)
     {
-
-        //TODO ARMOR ITEMS NEED TO BE FIXED
-        foreach ($tiers as $key => $tier) {
-            foreach ($tier['items'] as $key1 => $item) {
-                if (array_key_exists($key1, $tiered)){
-                    self::addItem(new UncommonTierItem($tiered[$key1][0], 0, $item['name'], $tiered[$key1][1], $item['bonuses']));
-                }
-
-            }
+        foreach (Utils::getItems() as $item) {
+            self::addItem($item);
         }
     }
 }
--- src/TheClimbing/RPGLike/Utils.php
+++ src/TheClimbing/RPGLike/Utils.php
@@ -9,7 +9,7 @@ use pocketmine\item\ItemIds;
 class Utils
 {
 
-
+    public static array $items;
     public static function parseKeywords(array $keywords, string $subject): string
     {
         $subject = str_replace(['{', '}'], [' ', ' '], $subject);
@@ -32,43 +32,52 @@ class Utils
         }
         return $messages;
     }
-
+    //TODO Those would ultimately all come from a config
+    public static function getItems() : array{
+        if (isset(self::$items)){
+            return self::$items;
+        }else{
+            $items = [
+                'stone_sword' => [ItemIds::STONE_SWORD, 0, 'Uncommon Sword', ['damage' => 1]],
+                'stone_axe' => [ItemIds::STONE_AXE, 0, 'Uncommon Axe', ['damage' => 1]],
+                'stone_pickaxe' => [ItemIds::STONE_PICKAXE, 0, 'Uncommon Pickaxe', ['damage' => 1]],
+                'stone_shovel' => [ItemIds::STONE_SHOVEL, 0, 'Uncommon Shovel', ['damage' => 1]],
+                'iron_sword' => [ItemIds::IRON_SWORD,0,'Rare Sword',['damage' => 1]],
+                'iron_axe' => [ItemIds::IRON_AXE,0,'Rare Axe', ['damage' => 1]],
+                'iron_pickaxe' => [ItemIds::IRON_PICKAXE,0,'Rare Pickaxe'.['damage' => 1]],
+                'iron_shovel' => [ItemIds::IRON_SHOVEL,0,'Rare Shovel',['damage' => 1]],
+                'gold_sword' => [ItemIds::GOLD_SWORD,0,'Mythic Sword',['damage' => 1]],
+                'gold_axe' => [ItemIds::GOLD_AXE,0,'Mythic Axe',['damage' => 1]],
+                'gold_pickaxe' => [ItemIds::GOLD_PICKAXE,0,'Mythic Pickaxe',['damage' => 1]],
+                'gold_shovel' => [ItemIds::GOLD_SHOVEL,0,'Mythic Shovel',['damage' => 1]],
+                'diamond_sword' => [ItemIds::DIAMOND_SWORD,0,'Epic Sword',['damage' =>1]],
+                'diamond_axe' => [ItemIds::DIAMOND_AXE,0,'Epic Axe',['damage' =>1]],
+                'diamond_pickaxe' => [ItemIds::DIAMOND_PICKAXE,0,'Epic Pickaxe',['damage' =>1]],
+                'diamond_shovel' => [ItemIds::DIAMOND_SHOVEL,0,'Epic Shovel',['damage' =>1]],
+                'iron_helmet' => [ItemIds::IRON_HELMET,0,'Uncommon Helmet',['defense' => 1]],
+                'iron_chestplate' => [ItemIds::IRON_CHESTPLATE,0,'Uncommon Chestplate',['defense' => 1]],
+                'iron_leggings' => [ItemIds::IRON_LEGGINGS,0,'Uncommon Leggings',['defense' => 1]],
+                'iron_boots' => [ItemIds::IRON_BOOTS,0,'Uncommon Boots',['defense' => 1]],
+                'gold_helmet' => [ItemIds::GOLD_HELMET,0,'Mythic Helmet',['defense' => 1]],
+                'gold_chestplate' => [ItemIds::GOLD_CHESTPLATE,0,'Mythic Chestplate',['defense' => 1]],
+                'gold_leggings' => [ItemIds::GOLD_LEGGINGS,0,'Mythic Leggings',['defense' => 1]],
+                'gold_boots' => [ItemIds::GOLD_BOOTS,0,'Mythic Boots',['defense' => 1]],
+                'diamond_helmet' => [ItemIds::DIAMOND_HELMET,0,'Rare Helmet',['defense' => 1]],
+                'diamond_chestplate' => [ItemIds::DIAMOND_CHESTPLATE,0,'Rare Chestplate',['defense' => 1]],
+                'diamond_leggings' => [ItemIds::DIAMOND_LEGGINGS,0,'Rare Leggings',['defense' => 1]],
+                'diamond_boots' => [ItemIds::DIAMOND_BOOTS,0,'Rare Boots',['defense' => 1]],
+                'chain_helmet' => [ItemIds::CHAIN_HELMET,0,'Epic Helmet',['defense' => 1]],
+                'chain_chestplate' => [ItemIds::CHAIN_CHESTPLATE,0,'Epic Chestplate',['defense' => 1]],
+                'chain_leggings' => [ItemIds::CHAIN_LEGGINGS,0,'Epic Leggings',['defense' => 1]],
+                'chain_boots' => [ItemIds::CHAIN_BOOTS,0,'Epic Boots',['defense' => 1]],
+            ];
+            self::$items = $items;
+            return $items;
+        }
+    }
     public static function translateStringToItem(string $item): array|null
     {
-        $items = [
-            'stone_sword' => [ItemIds::STONE_SWORD, 0, 'Uncommon Sword', ['damage' => 1]],
-            'stone_axe' => [ItemIds::STONE_AXE, 0, 'Uncommon Axe', ['damage' => 1]],
-            'stone_pickaxe' => [ItemIds::STONE_PICKAXE, 0, 'Uncommon Pickaxe', ['damage' => 1]],
-            'stone_shovel' => [ItemIds::STONE_SHOVEL, 0, 'Uncommon Shovel', ['damage' => 1]],
-            'iron_sword' => [ItemIds::IRON_SWORD,0,'Rare Sword',['damage' => 1]],
-            'iron_axe' => [ItemIds::IRON_AXE,0,'Rare Axe', ['damage' => 1]],
-            'iron_pickaxe' => [ItemIds::IRON_PICKAXE,0,'Rare Pickaxe'.['damage' => 1]],
-            'iron_shovel' => [ItemIds::IRON_SHOVEL,0,'Rare Shovel',['damage' => 1]],
-            'gold_sword' => [ItemIds::GOLD_SWORD,0,'Mythic Sword',['damage' => 1]],
-            'gold_axe' => [ItemIds::GOLD_AXE,0,'Mythic Axe',['damage' => 1]],
-            'gold_pickaxe' => [ItemIds::GOLD_PICKAXE,0,'Mythic Pickaxe',['damage' => 1]],
-            'gold_shovel' => [ItemIds::GOLD_SHOVEL,0,'Mythic Shovel',['damage' => 1]],
-            'diamond_sword' => [ItemIds::DIAMOND_SWORD,0,'Epic Sword',['damage' =>1]],
-            'diamond_axe' => [ItemIds::DIAMOND_AXE,0,'Epic Axe',['damage' =>1]],
-            'diamond_pickaxe' => [ItemIds::DIAMOND_PICKAXE,0,'Epic Pickaxe',['damage' =>1]],
-            'diamond_shovel' => [ItemIds::DIAMOND_SHOVEL,0,'Epic Shovel',['damage' =>1]],
-            'iron_helmet' => [ItemIds::IRON_HELMET,0,'Uncommon Helmet',['defense' => 1]],
-            'iron_chestplate' => [ItemIds::IRON_CHESTPLATE,0,'Uncommon Chestplate',['defense' => 1]],
-            'iron_leggings' => [ItemIds::IRON_LEGGINGS,0,'Uncommon Leggings',['defense' => 1]],
-            'iron_boots' => [ItemIds::IRON_BOOTS,0,'Uncommon Boots',['defense' => 1]],
-            'gold_helmet' => [ItemIds::GOLD_HELMET,0,'Mythic Helmet',['defense' => 1]],
-            'gold_chestplate' => [ItemIds::GOLD_CHESTPLATE,0,'Mythic Chestplate',['defense' => 1]],
-            'gold_leggings' => [ItemIds::GOLD_LEGGINGS,0,'Mythic Leggings',['defense' => 1]],
-            'gold_boots' => [ItemIds::GOLD_BOOTS,0,'Mythic Boots',['defense' => 1]],
-            'diamond_helmet' => [ItemIds::DIAMOND_HELMET,0,'Rare Helmet',['defense' => 1]],
-            'diamond_chestplate' => [ItemIds::DIAMOND_CHESTPLATE,0,'Rare Chestplate',['defense' => 1]],
-            'diamond_leggings' => [ItemIds::DIAMOND_LEGGINGS,0,'Rare Leggings',['defense' => 1]],
-            'diamond_boots' => [ItemIds::DIAMOND_BOOTS,0,'Rare Boots',['defense' => 1]],
-            'chain_helmet' => [ItemIds::CHAIN_HELMET,0,'Epic Helmet',['defense' => 1]],
-            'chain_chestplate' => [ItemIds::CHAIN_CHESTPLATE,0,'Epic Chestplate',['defense' => 1]],
-            'chain_leggings' => [ItemIds::CHAIN_LEGGINGS,0,'Epic Leggings',['defense' => 1]],
-            'chain_boots' => [ItemIds::CHAIN_BOOTS,0,'Epic Boots',['defense' => 1]],
-        ];
+        $items = self::getItems();
         if (array_key_exists($item, $items)){
             return $items[$item];
         }
